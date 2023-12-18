99a506b6ed001ab24e0c2cbb15297cf6b8ecac39
sandro <sandro.antonucci.7e7@itb.cat>
2023-12-18 13:58:44 +0100

    Primera versión del módulo assignNames, pendiente de revisión y testing.

---
+++ M03UF2PR1/GameModules/Modules.cs
@@ -0,0 +1,46 @@
+﻿using System.Data;
+
+namespace GameModules
+{
+    public class Modules
+    {
+
+        public static string[] assignNames()
+        {
+            const string MsgNames = "Introdueix els noms dels personatges (4) separats per comes i un espai: ";
+            const string MsgNamesNotValid = "Aquest format no és vàlid.";
+
+            string[] assignedNames = new string[4];
+            
+            string names;
+            bool namesValid = true;
+
+
+            do
+            {
+                Console.WriteLine(MsgNames);
+
+                names = Console.ReadLine();
+                names = names.Trim(',');
+
+
+                if (names.Split(' ').Length != 4)
+                {
+                    Console.Write(MsgNamesNotValid);
+                    namesValid = false;
+                }
+                else
+                {
+                    namesValid = true;
+                    assignedNames = names.Split(' ');
+                }
+
+            } while (!namesValid);
+
+            
+
+            return assignedNames;
+        }
+
+    }
+}
